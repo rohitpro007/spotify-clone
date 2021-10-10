e0dbc75e205d54e309af018b801ec474620345a9
rohitpro007 <72179695+rohitpro007@users.noreply.github.com>
2021-10-10 21:21:53 +0530

    Image of login page changed

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function Login() {
   return (
     <div className="login">
       <img
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        src="immages.jpg"
         alt=""
       />
       <a href={accessUrl}>LOGIN TO SPOTIFY</a>
